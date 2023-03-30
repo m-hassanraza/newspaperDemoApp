082441c768040c575237ee8ebf10fb9895295c81
M.Hassan Raza <54481751+m-hassanraza@users.noreply.github.com>
2023-03-30 16:49:06 +0500

    Project Setup

---
+++ NewspaperDemoApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NewspaperDemoApp
+//
+//  Created by Hassan Raza on 3/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    func wowowowossssdfsjdnkfjsdsdsdsdsdsdsdsdsdkjflsdjflksjdlkfjlskdjfwow() {
+    }
+}
---
+++ Pods/Target Support Files/Pods-NewspaperDemoApp/Pods-NewspaperDemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewspaperDemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_NewspaperDemoApp
+@end
